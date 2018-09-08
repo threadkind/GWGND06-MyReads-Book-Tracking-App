277977d2d915eda8679e3e26dbfb4008c3c6f929
threadkind <threadkind@gmail.com>
2018-09-07 20:14:42 -0500

    fix: Bug fix to display book covers

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -9,7 +9,7 @@ class ListBooks extends React.Component {
           <li key={book.id}>
             <div className="book">
               <div className="book-top">
-                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: book.imageLinks.thumbnail }}></div>
+                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
 	            <div className="book-shelf-changer">
 	              <select>
 	                <option value="move" disabled>Move to...</option>
