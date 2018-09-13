42b2ffb4570b095cadcc4213349c72d3fed3cc85
threadkind <threadkind@gmail.com>
2018-09-12 23:37:03 -0500

    fix: Add scroll to top when url changes and page view changes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
+import ScrollToTop from './ScrollToTop'
 import App from './App'
 import './index.css'
 
 ReactDOM.render(
 
-	<BrowserRouter><App /></BrowserRouter>,
-	document.getElementById('root')
+  <BrowserRouter>
+  	<ScrollToTop>
+      <App />
+    </ScrollToTop>
+  </BrowserRouter>,
+  document.getElementById('root')
 
 )
